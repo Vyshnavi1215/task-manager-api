922d80fc8bcdd76d6ccc648f4abb6842e99f4b65
Vyshnavi <48079248+Vyshnavi1215@users.noreply.github.com>
2020-11-01 19:24:32 +0530

    Update account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -9,7 +9,7 @@ sgMail.setApiKey(sendgridAPIKey)
 const sendWelcomeEmail = (email, name )=> {
     sgMail.send({
     to : email,
-    from: 'ramaniushahema@gmail.com',
+    from: 'XXXXXXX@gmail.com',
     subject: 'Thanks for joining in!',
     text : `Welcome to the app ,${name}. Let me  know how you can get along with the app`
 
@@ -21,7 +21,7 @@ const sendCancelationEmail = (email, name) =>{
 
     sgMail.send({
         to : email,
-        from :'ramaniushahema@gmail.com',
+        from :'XXXXXXX@gmail.com',
         subject : 'Sorry to see you go!',
         text : `Goodbye, ${name}. I hope to see you back sometime soon.`
     })
